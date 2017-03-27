2bcb42d124039215e892828432055ebb5ae34f9f
Andy Gaskell <andygaskelluk@yahoo.co.uk>
2017-03-27 20:59:15 +0100

    adding spin direction

--- motozero/spin_motors.sh
+++ motozero/spin_motors.sh
@@ -9,28 +9,44 @@ motor4=$4
 time=$5
 
 
-if [ $motor1 = "1" ]; then
+if [ $motor1 != "0" ]; then
 	# motor 1
+	gpio -g mode 27 out
+	gpio -g mode 24 out
+	gpio -g mode 5 out
+	echo "motor1 set"	
+
+
+	if [ $motor1 = "1" ]; then
+		echo "forward"	
+		gpio -g write 27 1
+		gpio -g write 24 0
+	else 
+		echo "backwards"
+		gpio -g write 27 0
+		gpio -g write 24 1
+	fi
+fi
+
+if [ $motor2 != "0" ]; then
+	# motor 2
 	gpio -g mode 22 out
 	gpio -g mode 6 out
 	gpio -g mode 17 out
 	
-	gpio -g write 22 1
-	gpio -g write 6 0
+
+	if [ $motor2 = "1" ]; then
+		gpio -g write 22 1
+		gpio -g write 6 0
+	else 
+		gpio -g write 22 0
+		gpio -g write 6 1
+	fi
+
 fi
 
-if [ $motor2 = "1" ]; then
-	# motor 2
-	gpio -g mode 27 out
-	gpio -g mode 24 out
-	gpio -g mode 5 out
-	
-	gpio -g write 27 1
-	gpio -g write 24 0
-fi
-
-if [ $motor1 = "1" ]; then
-	# motor 1
+if [ $motor1 != "0" ]; then
+	echo "motor1 go"
 	gpio -g write 5 1
 fi
 
@@ -45,8 +61,8 @@ fi
 sleep $time
 
 
-if [ $motor1 = "1" ]; then
-	# motor 1
+if [ $motor1 != "0" ]; then
+	echo "motor1 stop"
 	gpio -g write 5 0
 fi
 
