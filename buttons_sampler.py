0fed70a38626ca1c32b5a5bd57aa19ab6826272e
Andy Gaskell <andygaskelluk@yahoo.co.uk>
2017-09-19 22:35:36 +0000

    ok-working-4-button

--- buttons_sampler.py
+++ buttons_sampler.py
@@ -14,6 +14,8 @@ pygame.mixer.init()
 button_sounds = {
     Button(2): Sound("samples/Kurzweil-K2000-Dual-Bass-C1.wav"),
     Button(3): Sound("samples/Kurzweil-K2000-Steel-Str-Guitar-C4.wav"),
+    Button(4): Sound("samples/Kurzweil-K2000-Fair-Breath-C4.wav"),
+    Button(14): Sound("samples/Kurzweil-K2000-Tine-Elec-Piano-C5.wav"),
 }
 
 #while True:
