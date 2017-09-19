b9b8761280d581ec57243e8a0162fa12a6b403c2
Andy Gaskell <andygaskelluk@yahoo.co.uk>
2017-09-19 22:22:08 +0000

    ok

--- buttons_sampler.py
+++ buttons_sampler.py
@@ -11,12 +11,16 @@ from signal import pause
 
 pygame.mixer.init()
 
-drum = Sound("samples/Kurzweil-K2000-Dual-Bass-C1.wav")
+button_sounds = {
+    Button(2): Sound("samples/Kurzweil-K2000-Dual-Bass-C1.wav"),
+    Button(3): Sound("samples/Kurzweil-K2000-Steel-Str-Guitar-C4.wav"),
+}
 
 #while True:
 #    drum.play()
 
-button = Button(2)
-button.when_pressed = drum.play
+for button, sound in button_sounds.items():
+    button.when_pressed = sound.play
+
 pause()
 
