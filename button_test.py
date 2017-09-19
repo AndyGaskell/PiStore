7f4f469ec0284e3f1f5a1a665899470488dda80a
Andy Gaskell <andygaskelluk@yahoo.co.uk>
2017-09-19 21:50:25 +0000

    ok

---
+++ button_test.py
@@ -0,0 +1,17 @@
+#
+# http://razzpisampler.oreilly.com/ch07.html
+# button plugged into the 3rd and 5th
+#
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    input_state = GPIO.input(18)
+    if input_state == False:
+        print('Button Pressed')
+        time.sleep(0.2)
